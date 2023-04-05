7b8e1c411c5baaa0dd13195f1353dc6137c10794
Karol Kruszyński <karol.kruszynski0@gmail.com>
2023-04-05 22:32:59 +0200

    Changes: Switch to all eng named variables. Add: Ingredients list with print when dish_good == True      Empty def Add_Dish()

--- main.py
+++ main.py
@@ -8,36 +8,38 @@ import numpy as np
 #   TODO: Warunek if
 #   TODO: Wyświetlanie składników jeśli posiłek odpowiada.
 
-posilki = ["pomidorowa","grzybowa","serowa"]
-skladniki = {posilki[0]:"pomidory"}
-
-def powitanie():
+dishes = ["pomidorowa","grzybowa","serowa"]
+ingredients = ["cos"]
+def welcome():
     print("Witaj! Wybierz akcje poniżej wpisując odpowiednio 1 lub 2 \n 1. Wybierz Posiłek\n 2. Dodaj Posiłek")
     first_choose = int(input("Odpowiedź: "))
     return first_choose
 
-def wybor_posilku(first_choose):
+def dish_choice(first_choose):
     # print(first_choose)
-    print(len(posilki))
-    posilki_length = len(posilki)-1
-    print(posilki[random.randint(0,posilki_length)])
+    print(len(dishes))
+    dishes_length = len(dishes)-1
+    print(dishes[random.randint(0,dishes_length)])
 
-def posilek_is_good():
-    posilek_good = int(input("Czy wybrany posiłek Ci odpowiada ? Jeśli TAK wybierz 1 lub 2 jeśli NIE:  "))
-    if posilek_good == 1:
-        print(skladniki)
+def dish_is_good():
+    dish_good = int(input("Czy wybrany posiłek Ci odpowiada ? Jeśli TAK wybierz 1 lub 2 jeśli NIE:  "))
+    if dish_good == 1:
+        print(ingredients[0])
         return True
-    elif posilek_good == 2:
+    elif dish_good == 2:
         return False
     else:
         print("Invalid syntax!")
         return False
 
-first_choose = powitanie()
-if first_choose == 1:
-    wybor_posilku(first_choose)
+def add_dish():
 
-posilek_good = posilek_is_good()
-while posilek_good == False:
-    wybor_posilku(first_choose)
-    posilek_good = posilek_is_good()
+
+first_choose = welcome()
+if first_choose == 1:
+    dish_choice(first_choose)
+
+dish_good = dish_is_good()
+while dish_good == False:
+    dish_choice(first_choose)
+    dish_good = dish_is_good()
