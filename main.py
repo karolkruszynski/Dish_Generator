807e171f77da71e0482d000610386c32058568e1
Karol Kruszyński <karol.kruszynski0@gmail.com>
2023-04-07 12:37:54 +0200

    Add: Simple version of adding new dish to list Bug-fix: After Added Dish 'dish_good' still working. DONE

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 
 dishes = ["pomidorowa","grzybowa","serowa"]
 ingredients = ["cos"]
+dishes_length = len(dishes)-1
 def welcome():
     print("Witaj! Wybierz akcje poniżej wpisując odpowiednio 1 lub 2 \n 1. Wybierz Posiłek\n 2. Dodaj Posiłek")
     first_choose = int(input("Odpowiedź: "))
@@ -27,14 +28,25 @@ def dish_is_good():
         print("Invalid syntax!")
         return False
 
-# def add_dish():
+def add_dish(first_choose):
+    global dishes
+    new_dish_name = str(input("Podaj nazwe dania które chcesz wpisać do swojej listy: ")).lower()
+    print(new_dish_name)
+    print(type(new_dish_name))
+    print(dishes)
+    if new_dish_name in dishes:
+        print("To danie znajduje się już na liście.")
+    else:
+        dishes.append(new_dish_name)
+        print(dishes)
 
 
 first_choose = welcome()
 if first_choose == 1:
     dish_choice(first_choose)
-
-dish_good = dish_is_good()
-while dish_good == False:
-    dish_choice(first_choose)
     dish_good = dish_is_good()
+    while dish_good == False:
+        dish_choice(first_choose)
+        dish_good = dish_is_good()
+elif first_choose == 2:
+    add_dish(first_choose)
