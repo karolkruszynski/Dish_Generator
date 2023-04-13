e9b61ba931ad50df14b1c00df56290d285326d63
Karol Kruszyński <karol.kruszynski0@gmail.com>
2023-04-13 19:01:25 +0200

    Add: New function that store dishes with ingredients in dictionary and allow to add ingredients after dish was added to dictionary. Add: New function choosing() main start function Add: Recursion function when dish_good == 2 Add: New random choosing dish system fiting to the dictionary storage Add:

--- main.py
+++ main.py
@@ -1,59 +1,69 @@
 import random
-
 import numpy as np
 #   TODO: Selector między posiłkiem na 'słodko' lub 'słono'
 
-dishes = ["pomidorowa","grzybowa","serowa"]
-ingredients = ["cos"]
+dishes = {}
 dishes_length = len(dishes)-1
+choosed_dish = ""
 def welcome():
     print("Witaj! Wybierz akcje poniżej wpisując odpowiednio 1 lub 2 \n 1. Wybierz Posiłek\n 2. Dodaj Posiłek")
     first_choose = int(input("Odpowiedź: "))
     return first_choose
 
 def dish_choice(first_choose):
-    # print(first_choose)
+    global choosed_dish
     print(len(dishes))
-    dishes_length = len(dishes)-1
-    print(dishes[random.randint(0,dishes_length)])
+    print(dishes.keys())
+    dishes_list = []
+    for keys in dishes:
+        dishes_list.append(keys)
+        # print(dishes_list)
+    dishes_length = len(dishes_list)-1
+    print(type(dishes_length))
+    random_dish = random.randint(0,dishes_length)
+    print(random_dish)
+    choosed_dish = dishes_list[random_dish]
+    print(choosed_dish)
 
 def dish_is_good():
+    global choosed_dish
     dish_good = int(input("Czy wybrany posiłek Ci odpowiada ? Jeśli TAK wybierz 1 lub 2 jeśli NIE:  "))
     if dish_good == 1:
-        print(ingredients[0])
+        # print(choosed_dish)
+        print(dishes[choosed_dish])
         return True
     elif dish_good == 2:
-        return False
+        dish_choice(first_choose=1)
+        dish_is_good()
     else:
         print("Invalid syntax!")
         return False
 
-def add_dish(first_choose):
-    global dishes
-    new_dish_name = str(input("Podaj nazwe dania które chcesz wpisać do swojej listy: ")).lower()
-    print(new_dish_name)
-    print(type(new_dish_name))
-    print(dishes)
-    if new_dish_name in dishes:
-        print("To danie znajduje się już na liście.")
-    else:
-        #   TODO: Make a reapetable ver. with multi-ingredients in list and multi dish key values in dictionary
-        dishes.append(new_dish_name)
-        print(dishes)
-        new_ingredients_name = str(input("Podaj nazwe pierwszego składnika: ")).lower()
-        ingredients_list = []
-        lists = {new_dish_name: ingredients_list}
-        lists[new_dish_name].append(new_ingredients_name)
-        print(f"The new list named {new_dish_name} has been created.")
-        print(lists[new_dish_name])
 
+def dish_and_ingredients_add():
+    key = input("nazwa dania")
+    list_of_ingredients = input("skladniki")
+    def add_values_in_dict(dishes, key, list_of_ingredients):
+            ''' Append multiple values to a key in
+                    the given dictionary '''
+            if key not in dishes:
+                dishes[key] = list()
+            dishes[key].append(list_of_ingredients)
+            print(dishes)
+            return dishes
+    add_values_in_dict(dishes, key, list_of_ingredients)
 
-first_choose = welcome()
-if first_choose == 1:
-    dish_choice(first_choose)
-    dish_good = dish_is_good()
-    while dish_good == False:
-        dish_choice(first_choose)
-        dish_good = dish_is_good()
-elif first_choose == 2:
-    add_dish(first_choose)
+def choosing():
+    choose_loop = False
+    while choose_loop == False:
+        first_choose = welcome()
+        if first_choose == 1:
+            dish_choice(first_choose)
+            dish_good = dish_is_good()
+            while dish_good == False:
+                dish_choice(first_choose)
+                dish_good = dish_is_good()
+        elif first_choose == 2:
+            dish_and_ingredients_add()
+            print(choosed_dish)
+choosing()
